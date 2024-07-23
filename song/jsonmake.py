6d628a3e0fe1787c2fb31b57ebc886a5d83aa07b
angellsla <1509518916@qq.com>
2024-07-23 23:41:12 +0800

    使用-n参数传递保存的文件名

--- song/jsonmake.py
+++ song/jsonmake.py
@@ -1,20 +1,23 @@
 import os
 import json
+import argparse
 
-# 设置当前目录
+parser = argparse.ArgumentParser(description='Generate a JSON file with URLs of files in the current directory.')
+parser.add_argument('-n', '--filename', type=str, help='The name of the output JSON file', required=True)
+args = parser.parse_args()
+
 current_directory = os.getcwd()
 
-# 定义基础URL
 base_url = "https://cos1sistine-1313238216.cos.ap-shanghai.myqcloud.com/audio/loveshare/"
 
-# 遍历当前目录下的所有文件
 file_list = [file for file in os.listdir(current_directory) if os.path.isfile(os.path.join(current_directory, file))]
 
-# 为每个文件生成JSON格式的字符串
-json_list = [{"title": "", "url": base_url + file} for file in file_list]
+json_list = [{"title": "default", "url": base_url + file} for file in file_list]
 
-# 将JSON格式的字符串转换为JSON对象，并确保非ASCII字符被包含
 json_data = json.dumps(json_list, indent=4, ensure_ascii=False)
 
-# 打印生成的JSON数据
-print(json_data)
+output_file_path = os.path.join(current_directory, args.filename)
+with open(output_file_path, 'w', encoding='utf-8') as f:
+    f.write(json_data)
+
+print(f"JSON data has been written to {output_file_path}")
